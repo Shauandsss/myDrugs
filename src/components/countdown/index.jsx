5750165e78249588bd7d585b72d74d0b3df056cc
shauan santos <shauands@gmail.com>
2022-02-19 19:44:15 -0500

    Countdown

---
+++ src/components/countdown/index.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import './countdown.css'
+import hourGlass from './hourGlass_2.gif'
+
+export default () => {
+
+    const [days, setDays] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+
+    useEffect(() => {
+        const updateCountDown = () => {
+            const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+            let endDate = '03/04/2022';
+            const countDown = new Date(endDate).getTime();    
+            const now = new Date().getTime();
+            let distance = countDown - now;
+            setDays(Math.floor(distance / (day)))
+            setHours(Math.floor((distance % (day)) / (hour)))
+            setMinutes(Math.floor((distance % (hour)) / (minute)))
+            setSeconds(Math.floor((distance % (minute)) / second))
+        }
+        updateCountDown();
+
+        const intervalId = setInterval(() => { 
+            updateCountDown();
+        }, 1000);
+
+    }, [])
+
+    const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+    return (
+        <div className='container'>
+            <div className='hourglass'>
+                <img src={hourGlass}></img>
+            </div>
+            <div className='texts'>
+                <h2>Ready for</h2>
+                <h1>MyDrugs 2.0?</h1>
+                <div className='countDown'>
+                {zeroPad(days,2) + ' : ' + zeroPad(hours,2) + ' : ' + zeroPad(minutes,2) + " : " + zeroPad(seconds,2) } 
+                    <div className='countDown-label'>
+                        <div>
+                        D
+                        </div>
+                        <div>
+                        H
+                        </div> 
+                        <div>
+                        M
+                        </div>
+                        <div>
+                        S
+                        </div> 
+                    </div>
+                </div>
+                <h2>
+                    Coming Soon!
+                </h2>
+            </div>
+        </div>
+    )
+}
