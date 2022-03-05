96b3f259ff7d22d851bf52017dc440c48acfbaa6
shauan santos <shauands@gmail.com>
2022-03-04 22:38:34 -0500

    Homepage

--- src/components/homePage/index.jsx
+++ src/components/homePage/index.jsx
@@ -1,4 +1,6 @@
 import './homePage.css'
+import imageCem from '../../images/100.png'
+import ThreeMDMA from '../../images/3MDMA.png'
 
 export default function HomePage() {
     
@@ -47,6 +49,51 @@ export default function HomePage() {
                         Prices as low as 0.0013 BTC.
                     </div>
                 </div>
+            </div>
+
+            <div>
+                <img src={imageCem} className='imgCem'/>
+            </div>
+
+            <div className='HomePage-Mid-Titles'>
+                <div className='HomePage-Mid-Titles-1'>
+                    <div>
+                        Fresh from our factory.
+                    </div>
+                    <div>
+                        Constatly quality-checked by our experts.
+                    </div>
+                </div>
+                <div className='HomePage-Mid-Titles-2'>
+                    <div>
+                        MDMA in its purest form. 
+                    </div>
+                    <div>
+                        Delivery guaranted within 5 days.
+                    </div>                  
+                </div>
+            </div>
+
+            <div className='HomePage-Mid-White'>
+                <div>
+                    <div className='HomePage-Mid-White-Title'>
+                        An explosive new lineup.
+                    </div>
+                    <div className='HomePage-Mid-White-Subtitle' >
+                        For the best MDMA experience.<br/>
+                        Ever.
+                    </div>
+                    <div>
+                        Save 20% off your first order.
+                    </div>
+                    <div className='HomePage-Mid-White-ShopNow'>
+                        Shop Now &gt;
+                    </div>
+                </div>
+                <div>
+                    <img src={ThreeMDMA} />
+                </div>
+
             </div>
 
         </div>
